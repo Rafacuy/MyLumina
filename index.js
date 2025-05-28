ca0050050ba7a7c0cac82bb20eea6b7b4a93090f
Rafacuy <rafardhanganteng91@gmail.com>
2025-05-28 20:30:36 +0700

    Updated

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('./config/config');
-const lyraInteraction = require('./Lyra');
+const { initLyrabot, generateAIResponse } = require('./Lyra');
+const moodHelper = require('./utils/moodHelper');
+
+moodHelper.setAISummarizer(generateAIResponse); 
 
 const bot = new TelegramBot(config.telegramBotToken, { polling: true });
 
-lyraInteraction(bot);
+initLyrabot(bot);
