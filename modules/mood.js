2a089178be9914bda27205b33878fbb3fcfef3cc
Rafacuy <rafardhanganteng91@gmail.com>
2025-05-30 20:39:57 +0700

    Update v6 (Resource & Optimized AI)

---
+++ modules/mood.js
@@ -0,0 +1,14 @@
+// modules/mood.js
+
+
+const Mood = {
+    HAPPY: { emoji: '>.<', name: 'Senang' },
+    SAD: { emoji: ':)', name: 'Sedih' },
+    ANGRY: { emoji: '😠', name: 'Marah' },
+    LAZY: { emoji: '😪', name: 'Malas' },
+    LOVING: { emoji: '>///<', name: 'Loving' },
+    NORMAL: { emoji: '>~<', name: 'Normal' },
+    CALM: { emoji: '😌', name: 'Tenang' },
+};
+
+module.exports = Mood;
